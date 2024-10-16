8430a368ae30184c8a78d1be1a8dac2f36921af1
andreypoltev <andreypoltev@gmail.com>
2024-10-16 14:52:22 +0900

    init

---
+++ composeApp/src/iosMain/kotlin/ru/andreypoltev/moviescompose/MainViewController.kt
@@ -0,0 +1,5 @@
+package ru.andreypoltev.moviescompose
+
+import androidx.compose.ui.window.ComposeUIViewController
+
+fun MainViewController() = ComposeUIViewController { App() }
---
+++ composeApp/src/iosMain/kotlin/ru/andreypoltev/moviescompose/Platform.ios.kt
@@ -0,0 +1,9 @@
+package ru.andreypoltev.moviescompose
+
+import platform.UIKit.UIDevice
+
+class IOSPlatform: Platform {
+    override val name: String = UIDevice.currentDevice.systemName() + " " + UIDevice.currentDevice.systemVersion
+}
+
+actual fun getPlatform(): Platform = IOSPlatform()
